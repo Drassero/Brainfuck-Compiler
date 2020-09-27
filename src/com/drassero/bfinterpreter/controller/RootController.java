ae4781435a34ea5e3f57b44eacf565adad2ca478
Drassero <>
2020-09-27 12:48:14 +0100

    asynchronous task

--- src/com/drassero/bfinterpreter/controller/RootController.java
+++ src/com/drassero/bfinterpreter/controller/RootController.java
@@ -4,6 +4,8 @@ import com.drassero.bfinterpreter.Main;
 import com.drassero.bfinterpreter.util.History;
 import com.drassero.bfinterpreter.util.Interpreter;
 import javafx.animation.PauseTransition;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
@@ -11,7 +13,7 @@ import javafx.util.Duration;
 
 import java.util.Optional;
 
-public class RootController {
+public class  RootController {
 
     public static final int COLUMNS = 10;
 
@@ -26,35 +28,45 @@ public class RootController {
     @FXML
     private ListView<History> historyListView;
 
+    private Service<Void> runningService = new Service<>() {
+        @Override
+        protected Task<Void> createTask() {
+            return new Task<>() {
+                @Override
+                protected Void call() {
+                    Interpreter interpreter = new Interpreter(codeTextArea.getText(), inputTextArea.getText(), Main.BYTES_ARRAY_LENGTH);
+                    Optional<String> validStatement = interpreter.isStatementValid();
+                    if(validStatement.isPresent()) {
+                        sendLog(validStatement.get());
+                        return null;
+                    }
+                    Optional<String> validInput = interpreter.isInputValid();
+                    if(validInput.isPresent()) {
+                        sendLog(validInput.get());
+                        return null;
+                    }
+                    String output = interpreter.init().run();
+                    Optional<String> outputStatement = interpreter.isOutputValid();
+                    if(outputStatement.isPresent()) {
+                        sendLog(outputStatement.get());
+                        return null;
+                    }
+                    printText(output);
+                    return null;
+                }
+            };
+        }
+    };
     private PauseTransition clearLogTransition = new PauseTransition(Duration.seconds(4));
 
     @FXML
     private void initialize() {
-        clearLogTransition.setOnFinished(event -> {
-            outputTextArea.clear();
-        });
+        clearLogTransition.setOnFinished(event -> outputTextArea.clear());
     }
 
     @FXML
     private void onRun() {
-        Interpreter interpreter = new Interpreter(codeTextArea.getText(), inputTextArea.getText(), Main.BYTES_ARRAY_LENGTH);
-        Optional<String> validStatement = interpreter.isStatementValid();
-        if(validStatement.isPresent()) {
-            sendLog(validStatement.get());
-            return;
-        }
-        Optional<String> validInput = interpreter.isInputValid();
-        if(validInput.isPresent()) {
-            sendLog(validInput.get());
-            return;
-        }
-        String output = interpreter.init().run();
-        Optional<String> outputStatement = interpreter.isOutputValid();
-        if(outputStatement.isPresent()) {
-            sendLog(outputStatement.get());
-            return;
-        }
-        printText(output);
+        runningService.restart();
     }
 
     private void printText(String text) {
